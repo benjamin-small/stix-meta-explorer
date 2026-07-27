1fcd7bb3c72a51d844ae3b4892889034fadb8e4d
benjamin-small <benjamin.small83@gmail.com>
2026-07-27 09:27:25 -0400

    fix: remove unused test imports

--- src/types/__tests__/stix.test.ts
+++ src/types/__tests__/stix.test.ts
@@ -1,4 +1,4 @@
-import type { StixObjectType, Property, RelationshipDef, StixCategory } from '../stix';
+import type { StixCategory, StixObjectType } from '../stix';
 
 test('StixObjectType interface accepts valid object', () => {
   const obj: StixObjectType = {
